d3d01093f1e943bbe1ba114f810ba722872f788f
Vikas Jha <vks.jha@gmail.com>
2018-09-26 22:24:15 -0700

    Health Controller

--- src/main/java/com/bamm/service/controller/HealthController.java
+++ src/main/java/com/bamm/service/controller/HealthController.java
@@ -1,15 +1,18 @@
 package com.bamm.service.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bamm.service.constants.ResourceAPI;
 
-@Controller
+@RestController
 public class HealthController {
 	
+	/**
+	 * @return
+	 */
 	@GetMapping(ResourceAPI.HEALTH)
 	@ResponseStatus(HttpStatus.OK)
     public String health() {
